23bb10839d0f2e0b1eef443cc4a2529b3ac992ad
rafaelsalazarrojas <rafaelsalazarojas@gmail.com>
2025-02-25 17:50:25 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['katari-satelite-front-production.up.railway.app']
+  }
 })
